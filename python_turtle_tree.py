f05487b080f05b248f596a39a7ab9340cfc16b16
mostafa-36a2 <51724260+mostafa-36a2@users.noreply.github.com>
2022-07-31 13:42:26 +0300

    Create python_turtle_tree.py

---
+++ python_turtle_tree.py
@@ -0,0 +1,14 @@
+def tree(length):
+	rand = (random.random() -0.5)*6
+	rand_angle = (random.random() -0.5)*15
+	real_length = length + rand
+	width(length/10),go(real_length)
+	if length > 10 and random.random()>0.1:
+		turn(30+rand_angle )
+		tree(real_length -5)
+		turn(-30-rand_angle )
+	if length > 10 and random.random()>0.05:
+		turn(-30-rand_angle )
+		tree(real_length -5)
+		turn(30+rand_angle )
+	go(-real_length)
